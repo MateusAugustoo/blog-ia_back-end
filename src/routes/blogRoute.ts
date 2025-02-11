d2b406da0d036a5101cbb1410a05590b00d110fa
= <=>
2025-02-11 11:53:32 -0300

    ... in construct

--- src/routes/blogRoute.ts
+++ src/routes/blogRoute.ts
@@ -1,15 +1,28 @@
 import { z } from "zod";
-import type { FastifyRequest, FastifyReply } from "fastify";
 import { FastifyTypeInstance } from "../types/types";
+import { TopicService } from "../services/topicService";
 
 export const blogRoute = async (app: FastifyTypeInstance) => {
-  app.get('/topics', async (request: FastifyRequest, reply: FastifyReply) => {})
+  app.get('/topics', {
+    schema: {
+      tags: ['topics'],
+    }
+  }, async (request, reply) => {})
 
   app.post('/topics', {
     schema: {
+      tags: ['topics'],
       body: z.object({
         title: z.string()
       })
     }
-  }, async (request: FastifyRequest, reply: FastifyReply) => {})
+  }, async (request, reply) => {
+    try {
+      const { title } = request.body
+      const topic = await TopicService.create(title)
+      return reply.code(201).send(topic)
+    } catch (error) {
+      
+    }
+  })
 }
--- src/routes/topicRoute.ts
+++ src/routes/topicRoute.ts
@@ -1,4 +1,3 @@
-import { FastifyRequest, FastifyReply } from 'fastify'
 import { FastifyTypeInstance } from '../types/types'
 
 export const topicRoute = async (app: FastifyTypeInstance) => {}

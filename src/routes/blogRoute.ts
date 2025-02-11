243f1eb02441fe3434e7371886347343a4ecf121
= <=>
2025-02-11 01:10:51 -0300

    add topic routes with GET and POST endpoints in blogRoute

--- src/routes/blogRoute.ts
+++ src/routes/blogRoute.ts
@@ -1,6 +1,15 @@
+import { z } from "zod";
 import type { FastifyRequest, FastifyReply } from "fastify";
 import { FastifyTypeInstance } from "../types/types";
 
 export const blogRoute = async (app: FastifyTypeInstance) => {
+  app.get('/topics', async (request: FastifyRequest, reply: FastifyReply) => {})
 
+  app.post('/topics', {
+    schema: {
+      body: z.object({
+        title: z.string()
+      })
+    }
+  }, async (request: FastifyRequest, reply: FastifyReply) => {})
 }

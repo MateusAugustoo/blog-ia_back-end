7c25cb3719f8f3ac7e230d015d7cde1658b490cb
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2025-02-11 15:40:59 -0300

    feat: migrate blogRoute functionality to topicRoute and implement topic endpoints

--- src/routes/blogRoute.ts
+++ src/routes/blogRoute.ts
@@ -1,29 +1,3 @@
-import { z } from "zod";
 import { FastifyTypeInstance } from "../types/types";
-import { TopicService } from "../services/topicService";
 
-export const blogRoute = async (app: FastifyTypeInstance) => {
-  app.get('/topics', {
-    schema: {
-      tags: ['topics'],
-    }
-  }, async (request, reply) => {})
-
-  app.post('/topics', {
-    schema: {
-      tags: ['topics'],
-      description: 'Crie um novo tópico informando o title',
-      body: z.object({
-        title: z.string()
-      })
-    }
-  }, async (request, reply) => {
-    try {
-      const { title } = request.body
-      const topic = await TopicService.create(title)
-      return reply.code(201).send(topic)
-    } catch (error: any) {
-      return reply.code(500).send({ message: error.message })
-    }
-  })
-}
+export const blogRoute = async (app: FastifyTypeInstance) => {}
--- src/routes/topicRoute.ts
+++ src/routes/topicRoute.ts
@@ -1,3 +1,37 @@
-import { FastifyTypeInstance } from '../types/types'
+import { z } from "zod";
+import { FastifyTypeInstance } from "../types/types";
+import { TopicService } from "../services/topicService";
 
-export const topicRoute = async (app: FastifyTypeInstance) => {}
+export const topicRoute = async (app: FastifyTypeInstance) => {
+  app.get(
+    "/topics",
+    {
+      schema: {
+        tags: ["topics"],
+      },
+    },
+    async (request, reply) => {}
+  );
+
+  app.post(
+    "/topics",
+    {
+      schema: {
+        tags: ["topics"],
+        description: "Crie um novo tópico informando o title",
+        body: z.object({
+          title: z.string(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      try {
+        const { title } = request.body;
+        const topic = await TopicService.create(title);
+        return reply.code(201).send(topic);
+      } catch (error: any) {
+        return reply.code(500).send({ message: error.message });
+      }
+    }
+  );
+};

a7e4c7e7a50327f123803dbe980cdf007e32e027
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2025-02-12 18:22:20 -0300

    style: format code for consistency and readability in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,46 @@
-import Fastify from 'fastify'
-import { fastifyCors } from '@fastify/cors'
-import { fastifySwagger } from '@fastify/swagger'
-import { fastifySwaggerUi } from '@fastify/swagger-ui'
-import { ZodTypeProvider, validatorCompiler, serializerCompiler, jsonSchemaTransform } from 'fastify-type-provider-zod'
-import { topicRoute } from './routes/topicRoute'
+import Fastify from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import {
+  ZodTypeProvider,
+  validatorCompiler,
+  serializerCompiler,
+  jsonSchemaTransform,
+} from "fastify-type-provider-zod";
+import { topicRoute } from "./routes/topicRoute";
 
-const app = Fastify({ logger: true }).withTypeProvider<ZodTypeProvider>()
+const app = Fastify({ logger: true }).withTypeProvider<ZodTypeProvider>();
 
-app.setValidatorCompiler(validatorCompiler)
-app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-app.register(fastifyCors, { origin: '*' })
+app.register(fastifyCors, { origin: "*" });
 
 app.register(fastifySwagger, {
   openapi: {
     info: {
-      title: 'Blog API',
-      version: '1.0.0',
-    }
+      title: "Blog API",
+      version: "1.0.0",
+    },
   },
-  transform: jsonSchemaTransform
-})
+  transform: jsonSchemaTransform,
+});
 
 app.register(fastifySwaggerUi, {
-  routePrefix: '/docs',
-})
+  routePrefix: "/docs",
+});
 
-app.get('/', async (request, reply) => {
-  return { message: 'hello world!!' }
-})
-
-app.register(topicRoute)
+app.register(topicRoute);
 
 const start = async () => {
   try {
-    await app.listen({port: 3333})
-    console.log('server running on port 3333')
+    await app.listen({ port: 3333 });
+    console.log("server running on port 3333");
   } catch (err) {
-    app.log.error(err)
-    process.exit(1)
+    app.log.error(err);
+    process.exit(1);
   }
-}
+};
 
-start()
+start();

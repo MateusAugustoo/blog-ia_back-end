3ee612f92091f4a9ae414d1068c163ad2afaac1a
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2025-02-11 23:16:04 -0300

    feat: enhance topicRoute with getAll and delete endpoints, and improve error handling

--- src/routes/topicRoute.ts
+++ src/routes/topicRoute.ts
@@ -8,9 +8,18 @@ export const topicRoute = async (app: FastifyTypeInstance) => {
     {
       schema: {
         tags: ["topics"],
+        description: "Liste todos os tópicos existentes",
       },
     },
-    async (request, reply) => {}
+    async (request, reply) => {
+      try {
+        const topics = await TopicService.getAll();
+
+        return reply.code(200).send(topics);
+      } catch (error: Error | any) {
+        return reply.code(500).send({ message: error.message });
+      }
+    }
   );
 
   app.post(
@@ -18,18 +27,55 @@ export const topicRoute = async (app: FastifyTypeInstance) => {
     {
       schema: {
         tags: ["topics"],
-        description: "Crie um novo tópico informando o title",
+        description:
+          "Crie um novo tópico informando o title, cada title e único",
         body: z.object({
           title: z.string(),
         }),
       },
     },
     async (request, reply) => {
+
+      const titlesExist = await TopicService.getAll();
+
+      if (!request.body.title) {
+        return reply.code(400).send({ message: "Title is required" });
+      }
+      
       try {
         const { title } = request.body;
+
+        if (titlesExist.some(topic => topic.title === title)) {
+          return reply.code(400).send({ message: "Title already exists" });
+        }
+
         const topic = await TopicService.create(title);
         return reply.code(201).send(topic);
-      } catch (error: any) {
+
+      } catch (error: Error | any) {
+        return reply.code(500).send({ message: error.message });
+      }
+    }
+  );
+
+  app.delete(
+    "/topics/:id",
+    {
+      schema: {
+        tags: ["topics"],
+        description: "Deleta um tópico informando o id",
+        params: z.object({
+          id: z.string(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      try {
+        const { id } = request.params;
+        await TopicService.delete(id);
+
+        return reply.code(204).send();
+      } catch (error: Error | any) {
         return reply.code(500).send({ message: error.message });
       }
     }

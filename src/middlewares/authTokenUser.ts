c1f060faf888eaf3664430016ad6c7c310125a7c
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2025-02-24 15:25:35 -0300

    feat: implement authentication middleware to validate user tokens

--- src/middlewares/authTokenUser.ts
+++ src/middlewares/authTokenUser.ts
@@ -1,5 +1,18 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { auth } from '../firebase'
 
+export async function authMiddleware(request: FastifyRequest, reply: FastifyReply) {
+  const token = request.headers.authorization?.split(' ')[1];
 
-export async function authMiddleware() {}
+  if (!token) {
+    reply.status(401).send({ message: 'Token not provided' });
+    return;
+  }
+
+  try {
+    const decodedToken = await auth.verifyIdToken(token)
+    request.user = decodedToken;
+  } catch (error) {
+    reply.status(401).send({ message: 'Invalid token' });
+  }
+}

5804f0e4976906d985ae6c772853cb26f2019f91
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2025-03-11 19:47:06 -0300

    feat: verify object .env configs firebase

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
-  FIREBASE_CREDENTIALS: z.string(),
+  FIREBASE_CREDENTIALS: z.object({}),
 })
 
 export const env = envSchema.parse(process.env)
